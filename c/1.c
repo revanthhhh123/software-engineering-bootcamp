3e13442cb7ba62f1974b9551a5955751fc9092b9
revanthhhh123 <revanthhhh123@gmail.com>
2026-07-14 23:39:06 +0530

    day06

---
+++ c/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //printf("Hello, World!\n");
+    //int a=4;
+    /*char b="qw";
+    float c=2.33;*/
+    int age=22;
+    printf(" age is %d",age);
+    
+    return 0;
+}
---
+++ c/2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    // int q;
+    // int w;
+    // scanf("%d",&q);
+    // scanf("%d",&w);
+    // printf("%d\n",q);
+    // printf("%d\n",w);
+    // return 0;
+    // int a,b;
+    // a=b=2;
+    // double power= pow(a,b);
+    // int a,b;
+    // a=b=2;
+    // printf("%d\n",a%b);
+    // printf("%d\n",a/b);
+    // float a=1;
+    // printf("%d",a);
+    // int a;
+    // scanf("%d",&a);
+    // int  c=(a%2==0);
+    // printf("%d",c);
+    int a;
+    scanf("%d",&a);
+    if(a>=18){
+        printf("adult");
+
+    }
+    else{
+        printf("not");
+    }
+    return 0;
+
+}
+
