4fe4b8207d19fdf932abe4783dc0bfefa9aea7cb
revanthhhh123 <revanthhhh123@gmail.com>
2026-07-15 23:22:32 +0530

    day07

---
+++ day-07/c/1.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+void fr();
+void goodbye();
+int main(){
+    // int a,b ;
+    // a=b=1;
+    // a==b?printf("true"):printf("false");
+    // int day;
+    // scanf("%d",&day);
+    // switch(day){
+    //     case 1:printf("qw");
+    //             break;
+    //     case 2:printf("er");
+    //             break;
+    //     default:printf("dd");
+        // for(int i=0;i<=10;i+=1){
+        //     printf("%d\n",i);
+        // }
+//         int a;
+//         scanf("%d",&a);
+//         int i=0;
+//         while(i<=a){
+//             printf("%d,",i);
+//             i++;
+//         }
+        // for(int i=0;i<=10;i++){
+        //     if(i==5){
+        //         break;
+        //     }
+        //     printf("%d\n",i);
+        // }
+        // int a;
+        // scanf("%d",&a);
+        // int i=0;
+        // int sum=0;
+        // do{
+            
+        //     sum+=i;
+        
+        //     i++;
+        // }while(i<=a);
+        // printf("Sum: %d", sum);
+        // int a;
+        // scanf("%d",&a);
+        // int factor=1;
+        // for(int i=a;i>=1;i--){
+        //     factor*=i;
+        // }
+        // printf("Factorial: %d",factor);
+        void fr();
+        void goodbye();
+}
+void fr(){
+    printf("Hello");
+}
+void goodbye(){
+    printf("Goodbye");
+}
